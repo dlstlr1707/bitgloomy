042290c7e67019454c746cc0a1a3c81f00588973
dlstlr1707 <49482052+dlstlr1707@users.noreply.github.com>
2025-01-27 16:54:14 +0900

    1.7.1 1/27 save

--- src/page/FindUserInfo.js
+++ src/page/FindUserInfo.js
@@ -2,58 +2,13 @@ import {useEffect, useState} from "react";
 import "../css/findUserInfo.css";
 import Footer from "./Footer";
 function FindUserInfo() {
-    const [selectedTab, setSelectedTab] = useState("");
+    const [selectedTab, setSelectedTab] = useState("findId");
     const handleClickP = (e) => {
         setSelectedTab(e.target.id);
     }
-    const renderFindUserInput = () => {
-        if (selectedTab === "findId"||selectedTab==="") {
-            return (
-                <div id="findUserInputDiv">
-                    <div class="inputBoxDiv">
-                        <p>NAME</p>
-                        <input type="text"/>
-                    </div>
-                    <div class="inputBoxDiv">
-                        <p>E-MAIL</p>
-                        <input type="text">
-                            <button>인증</button>
-                        </input>
-                    </div>
-                    <div class="inputBoxDiv">
-                        <p>인증번호</p>
-                        <input type="text"/>
-                    </div>
-                    <button>아이디 찾기</button>
-                </div>
-            );
-        } else {
-            <div id="findUserInputDiv">
-                <div class="inputBoxDiv">
-                    <p>ID</p>
-                    <input type="text"/>
-                </div>
-                <div class="inputBoxDiv">
-                    <p>NAME</p>
-                    <input type="text"/>
-                </div>
-                <div class="inputBoxDiv">
-                    <p>E-MAIL</p>
-                    <input type="text">
-                        <button>인증</button>
-                    </input>
-                </div>
-                <div class="inputBoxDiv">
-                    <p>인증번호</p>
-                    <input type="text"/>
-                </div>
-                <button>비밀번호 찾기</button>
-            </div>
-        }
-    }
-    useEffect(()=> {
-        console.log("selectedTab is : "+ selectedTab);
-    },[selectedTab]);
+    useEffect(() => {
+        console.log("selectedTab is : " + selectedTab);
+    }, [selectedTab]);
     return (
         <div>
             <main>
@@ -62,7 +17,48 @@ function FindUserInfo() {
                         <p id="findId" onClick={handleClickP}>아이디 찾기</p>
                         <p id="findPw" onClick={handleClickP}>비밀번호 찾기</p>
                     </div>
-                    {()=> {renderFindUserInput();}}
+                    {
+                        selectedTab === "findId" &&< div id = "findUserInputDiv" > <div class="inputBoxDiv">
+                                <p>NAME</p>
+                                <input type="text"/>
+                            </div>
+                            <div class="inputBoxDiv">
+                                <p>E-MAIL</p>
+                                <div id="emailInputBoxDiv">
+                                    <input type="text"/>
+                                    <button>인증</button>
+                                </div>
+                            </div>
+                            <div class="inputBoxDiv">
+                                <p>인증번호</p>
+                                <input type="text"/>
+                            </div>
+                            <button>아이디 찾기</button>
+                        </div>
+                    }
+                    {
+                        selectedTab === "findPw" &&< div id = "findUserInputDiv" > <div class="inputBoxDiv">
+                                <p>ID</p>
+                                <input type="text"/>
+                            </div>
+                            <div class="inputBoxDiv">
+                                <p>NAME</p>
+                                <input type="text"/>
+                            </div>
+                            <div class="inputBoxDiv">
+                                <p>E-MAIL</p>
+                                <div id="emailInputBoxDiv">
+                                    <input type="text"/>
+                                    <button>인증</button>
+                                </div>
+                            </div>
+                            <div class="inputBoxDiv">
+                                <p>인증번호</p>
+                                <input type="text"/>
+                            </div>
+                            <button>비밀번호 찾기</button>
+                        </div>
+                    }
                 </div>
             </main>
             <Footer/>
